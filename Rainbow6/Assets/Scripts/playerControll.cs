fa412cb16e5552a9139a5f4e1406a7d39b57dc28
vegil00 <30295230+vegil00@users.noreply.github.com>
2017-10-29 12:00:30 +0800

    change To TPS

--- Rainbow6/Assets/Scripts/playerControll.cs
+++ Rainbow6/Assets/Scripts/playerControll.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerControll : MonoBehaviour {
-
+    public float moveSpeed;
+    Rigidbody rigid;
+    float curSpeed;
 	// Use this for initialization
 	void Start () {
-		
+        rigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if(v!=0||h!=0)
+        {
+            Vector3 dir = new Vector3(h, 0, v).normalized;
+
+            rigid.MovePosition(transform.position + dir * moveSpeed);
+            curSpeed = Mathf.Lerp(curSpeed, moveSpeed, Time.deltaTime);
+        }
+        else
+        {
+            curSpeed = 0;
+        }
+        GetComponent<Animator>().SetFloat("Speed", curSpeed);
+      
 	}
 }
